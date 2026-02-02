294a2a734bba5421628cf29eecd4987d85935e0a
nico9905 <nico9lippl@gmail.com>
2026-02-02 20:36:40 +0100

    kommentare lightcontrol

--- ui/control_light.py
+++ ui/control_light.py
@@ -15,12 +15,12 @@ def control_light(room, action, user):
     handshape: erkannte Geste
     user: aktueller Benutzer
     """
-    if action == "up":
+    if action == "up":                                                  # wenn Aktion up, dann Licht plus einen Step
         room.light_level = clamp_light(room.light_level + STEP)
-    elif action == "down":
+    elif action == "down":                                              # wenn Aktion down, dann Licht minus einen Step
         room.light_level = clamp_light(room.light_level - STEP)
-    elif action == "on":
+    elif action == "on":                                                # wenn Aktion on, dann Licht komplett an
         room.light_level = 100
-    elif action == "off":
+    elif action == "off":                                               # wenn Aktion off, dann Licht komplett aus
         room.light_level = 0
     write_log(user, f"{room.name}: Light {action}, now {room.light_level}%")
